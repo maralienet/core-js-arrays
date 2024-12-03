ea0b6f60fc6912fa2ca1f51272d4f86b2f355b86
maralienet <maralienet@gmail.com>
2024-12-03 19:17:33 +0300

    feat: complete function createChunks

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -345,8 +345,15 @@ function calculateBalance(arr) {
  *    createChunks(['a', 'b', 'c', 'd', 'e'], 2) => [['a', 'b'], ['c', 'd'], ['e']]
  *    createChunks([10, 20, 30, 40, 50], 1) => [[10], [20], [30], [40], [50]]
  */
-function createChunks(/* arr, chunkSize */) {
-  throw new Error('Not implemented');
+function createChunks(arr, chunkSize) {
+  const res = arr.reduce((newArr, a, i) => {
+    if (i % chunkSize === 0) {
+      newArr.push([]);
+      newArr[newArr.length - 1].push(a);
+    } else newArr[newArr.length - 1].push(a);
+    return newArr;
+  }, []);
+  return res;
 }
 
 /**
