2953ee501c143c27e33d127af886a9c1c7824ce9
maralienet <maralienet@gmail.com>
2024-12-03 19:23:55 +0300

    feat: complete function generateOdds

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -368,8 +368,10 @@ function createChunks(arr, chunkSize) {
  *    generateOdds(2) => [ 1, 3 ]
  *    generateOdds(5) => [ 1, 3, 5, 7, 9 ]
  */
-function generateOdds(/* len */) {
-  throw new Error('Not implemented');
+function generateOdds(len) {
+  return Array(len)
+    .fill(1)
+    .map((a, i) => a + 2 * i);
 }
 
 /**
