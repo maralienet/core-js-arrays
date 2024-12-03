4c1edb91139859efa799b88ec927a7b3aafcfd36
maralienet <maralienet@gmail.com>
2024-12-03 16:03:28 +0300

    feat: complete function findAllOccurrences

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -79,8 +79,13 @@ function findElement(arr, value) {
  *    findAllOccurrences([ null, undefined, null ], null) => 2
  *    findAllOccurrences([ true, 0, 1, 'true' ], true) => 1
  */
-function findAllOccurrences(/* arr, item */) {
-  throw new Error('Not implemented');
+function findAllOccurrences(arr, item) {
+  let c = 0;
+  arr.map((a) => {
+    if (a === item) c += 1;
+    return a;
+  });
+  return c;
 }
 
 /**
