82b0a4cad01cdc66bf3d82887c355a26069d1d60
maralienet <maralienet@gmail.com>
2024-12-04 12:52:44 +0300

    feat: complete function findCommonElements

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -497,8 +497,14 @@ function getMaxItems(arr, n) {
  *    findCommonElements(['a', 'b', 'c'], ['b', 'c', 'd']) => [ 'b', 'c' ]
  *    findCommonElements([1, 2, 3], ['a', 'b', 'c']) => []
  */
-function findCommonElements(/* arr1, arr2 */) {
-  throw new Error('Not implemented');
+function findCommonElements(arr1, arr2) {
+  const arr = arr1.map((item1) => {
+    return arr2.map((item2) => {
+      if (item1 === item2) return item1;
+      return null;
+    });
+  });
+  return arr.flat().filter((item) => item !== null);
 }
 
 /**
