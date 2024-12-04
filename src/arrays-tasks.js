26b0b5b2cb8cc9fb5c08e2344344f05ac6a6f45a
maralienet <maralienet@gmail.com>
2024-12-04 12:26:46 +0300

    feat: complete function getHexRGBValues

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -460,8 +460,10 @@ function getIndicesOfOddNumbers(numbers) {
  *    getHexRGBValues([ 0, 255, 16777215]) => [ '#000000', '#0000FF', '#FFFFFF' ]
  *    getHexRGBValues([]) => []
  */
-function getHexRGBValues(/* arr */) {
-  throw new Error('Not implemented');
+function getHexRGBValues(arr) {
+  if (!arr.length) return [];
+  const hexes = arr.map((item) => item.toString(16).toUpperCase());
+  return hexes.map((item) => `#${item.padStart(6, 0)}`);
 }
 
 /**
