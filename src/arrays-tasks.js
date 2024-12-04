0d94364550426cc24f4c085057ea42014be28b2a
maralienet <maralienet@gmail.com>
2024-12-04 12:17:16 +0300

    feat: complete function getIndicesOfOddNumbers

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -445,8 +445,9 @@ function getIdentityMatrix(n) {
  *    getIndicesOfOddNumbers([2, 4, 6, 8, 10]) => []
  *    getIndicesOfOddNumbers([11, 22, 33, 44, 55]) => [0, 2, 4]
  */
-function getIndicesOfOddNumbers(/* numbers */) {
-  throw new Error('Not implemented');
+function getIndicesOfOddNumbers(numbers) {
+  const arr = numbers.map((item, i) => (item % 2 === 0 ? null : i));
+  return arr.filter((item) => item !== null);
 }
 
 /**
