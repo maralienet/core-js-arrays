25b4121ce6e54678155e336644f86f06761c0792
maralienet <maralienet@gmail.com>
2024-12-04 12:11:06 +0300

    feat: complete function getIdentityMatrix

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -424,8 +424,14 @@ function getFalsyValuesCount(arr) {
  *                              [0,0,0,1,0],
  *                              [0,0,0,0,1]]
  */
-function getIdentityMatrix(/* n */) {
-  throw new Error('Not implemented');
+function getIdentityMatrix(n) {
+  return Array(n)
+    .fill(0)
+    .map((a, i) =>
+      Array(n)
+        .fill(0)
+        .map((b, j) => 1 - Math.min(Math.abs(i - j), 1))
+    );
 }
 
 /**
